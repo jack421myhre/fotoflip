fc7ffa68889e92a3950047c97461ea2357ebf84b
Jackson Myhre <jack421myhre@gmail.com>
2022-10-09 22:57:11 -0500

    Starter files. Includes js,css,html,images, and CDN for Vue. Basic layout starting with Vue. Learning Vue and building this alongside.

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data: function () {
+        return {
+            artistOne: 'by Alfonso',
+            titleOne: 'Foggybrain',
+            imageOne: './assets/images/michael-mouritz-unsplash.jpg',
+            artistTwo: 'by Rachel',
+            titleTwo: 'Plaidweather',
+            imageTwo: './assets/images/daniel-j-schwarz-unsplash.jpg'
+        };
+    }
+});
